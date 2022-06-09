ae1b962b3172279829d22725886d922b6f94f7a0
Feilian Huang <fhuang49@gatech.edu>
2022-06-09 15:26:59 -0500

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -233,14 +233,58 @@ allFramesInfoPath = os.path.join(output_dir, 'allFramesInfo.log')
 offset= [] # IDR byteoffset
 frame = [] # IDR Sample number 
 
+audioSize = [] #[size]
+
 with open(allFramesInfoPath, 'r') as file:
     lines = file.read().splitlines()
     for row in lines:
         if "stream 0" in row and "keyframe 1" in row:
             frame.append(int(row.split(',')[6].split(' ', 2)[2]))
             offset.append(int(row.split(',')[7].split(' ', 2)[2], 16))
+        if "stream 1" in row:
+            audioSize.append(int(row.split(',')[9].split(' ', 2)[2]))
+            #c = int(row.split(',')[6].split(' ', 2)[2])
+            #audioSize.append([b, c])
+
 
 file.close()
+
+
+############################ Audio Grouping #######################################################
+targetSize = 4500000  #4.5MB
+overlap = 780         #大约五秒？
+cutPlan = []
+
+sum = 0
+start = 0
+i = 0
+while(i < len(audioSize)):
+    overlap = 780    #大约五秒？
+    sum = sum + audioSize[i]
+    if (sum >= targetSize):
+        cutPlan.append([start, i])
+        sum = 0
+        minusOffset = 0
+        while(overlap > 0):
+            overlap = overlap - audioSize[i + minusOffset]
+            minusOffset = minusOffset + 1
+        start = i + 1 - minusOffset
+        i = start - 1
+        print(start)
+    i = i + 1
+    
+if(start != len(audioSize) - 1):
+    cutPlan.append([start,len(audioSize) - 1])
+print(cutPlan)
+
+
+
+
+
+
+
+
+
 
 print('Success in getting frame, offset of IDR')
 
